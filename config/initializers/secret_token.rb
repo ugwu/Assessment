075c3596107a667b9b9b2797a9c32beb3e4a5aae
Patrick <patrick.ugwu@mit-can.com>
2011-04-04 20:00:11 +0100

    Clean set up for Project Assessment 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assessment::Application.config.secret_token = '64cc048ca882c4e5ee278bfffba21e5b14d54f12fd5c52e030828a4d5271fed6ad731d95925e76ddd2d4781e210d67583154fbfe3352e8eeffc0d86778bf2bd3'
